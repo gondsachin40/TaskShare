3a5cdccad0e79d53c8afb1c8524f8dfccca51ad4
alfaiz-exe <alfaizkhan407@gmail.com>
2025-09-28 10:43:42 +0530

    updated tasks here taskId -> objectId of objectiveModel

---
+++ models/task.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import Objective from "./objective";
+const { Schema } = mongoose;
+
+const taskSchema = new Schema({
+    taskTitle: {
+        type: String,
+        required:true,
+    },
+    taskDescription: {
+        type: String,
+        required: true,
+    },
+    links:{
+       type: [String],
+
+    },
+    tasksId: {
+        type: Schema.Types.ObjectId,
+        required:true
+    }
+    ,
+}, { timestamps: true });
+
+const task = mongoose.model("tasks", taskSchema);
+
+export default task;
