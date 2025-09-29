67e054c8259cd4a4033f2d66818e100de7b9b1d7
Alfaiz Khan <136231638+alfaiz-exe@users.noreply.github.com>
2025-09-29 12:16:08 +0530

    update

--- Alfaiz/src/app/app.component.ts
+++ Alfaiz/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { log } from 'console';
 
+@Component ({
+  selector: 'app-login',
+  standalone:true,
+  templateUrl:'./login/login.component.html',
+  styleUrl:'./login/login.component.css'
+})export class login{
+  
+}
+@Component({
+  selector:'sign-up',
+  templateUrl:'./sign-up/sign-up.component.html',
+  styleUrl:'./sign-up/sign-up.component.css'
+})export class signUp{
+  
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,login,signUp],
+  templateUrl:'./app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
