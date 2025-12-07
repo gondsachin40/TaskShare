47145571eac8167d208ef6e3d09b2a9f8d927096
Alfaiz Khan <136231638+alfaiz-exe@users.noreply.github.com>
2025-12-07 11:22:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ import cors from 'cors'
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-    origin: 'https://frontendoftaskshare.onrender.com/',  // Allow requests only from this origin
+app.use(cors({  // Allow requests only from this origin
     methods: ['GET', 'POST'],        // Allow only GET and POST methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow only 'Content-Type' header
     credentials: true
